8f13d13fc3d17cea900473f0b8490ce7f0c39afe
KiselevP <kiselev.random@yandex.ru>
2023-04-27 16:57:37 +0300

    ДЗ-7

---
+++ src/main/java/homework/lesson7/Main.java
@@ -0,0 +1,10 @@
+package homework.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+        userInterfaceView.runInterface();
+
+    }
+}
---
+++ src/main/java/homework/lesson7/UserInterfaceView.java
@@ -0,0 +1,36 @@
+package homework.lesson7;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UserInterfaceView {
+    private Controller controller = new Controller();
+
+    public void runInterface() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(
+                    "1 - погода на сегодня; " +
+                    "5 - погода на 5 дней; " +
+                    "Для выхода введите 0:");
+
+            String command = scanner.nextLine();
+
+            if (command.equals("0")) break;
+
+            if (command.equals("1") || command.equals("5")) {
+                try {
+                    System.out.println("Введите имя города: ");
+                    String city = scanner.nextLine();
+                    controller.getWeather(command, city);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("ВВедена некорректная команда, повторите ввод");
+                System.out.println();
+            }
+        }
+    }
+}
